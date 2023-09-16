75322385eec54e8fc5de20e7edcef9e8e1d3fad6
wenderf5 <wenderfabianoalves@gmail.com>
2023-09-16 18:41:45 -0300

    1.1

--- Script/script.js
+++ Script/script.js
@@ -5,39 +5,51 @@ google.charts.setOnLoadCallback(desenharGrafico);
 // Array de dados inicial
 var dados = [
   ['Tarefa', 'Horas por Dia'],
-  ['Dormir', 8],
-  ['Trabalho', 8],
-  ['Lazer', 5],
-  ['alimentação', 2],
-  ['Academia', 1],
 ];
 
+function deletar(id) {
+  var tarefa = document.getElementById(id);
+  tarefa.remove();
+
+  deletararray(id);
+}
+function deletararray(id) {
+  dados[id] =  [undefined, undefined]; // Remove o item do array com base no índice
+  desenharGrafico(); // Atualiza a lista de itens na página
+}
+
+let contador = 0;
+let i = 0;
 
 
-// Função para adicionar um novo item ao array de dados
-function adicionarItem() {
-  var box1valor = document.getElementById('box1').value;
-  let input = document.getElementById("box2").value;
-  let main = document.getElementById("arealista");
-  let valorinicial = document.getElementById ("valorinicial").value;
-  let valorinicial2 = parseInt(valorinicial);
-console.log(valorinicial)
-  if (box1valor.trim() !== '') {
-    dados.push([box1valor, valorinicial2]); // Adiciona um novo item com 2 horas (ou o valor desejado)
-    desenharGrafico(); // Redesenha o gráfico com os dados atualizados
-    document.getElementById('box1').value = ''; // Limpa o campo de entrada
-    document.getElementById('box2').value = ''; // Limpa o campo de entrada
-    if (datavalor == "2") { datavalor = "1" };
-    data5.dataset.aberto = datavalor;
-    let novaatividade = `<div class="tarefas">
+  // Função para adicionar um novo item ao array de dados
+  function adicionarItem() {
+    var box1valor = document.getElementById('box1').value;
+    let input = document.getElementById("box2").value;
+    let main = document.getElementById("arealista");
+    let valorinicial = document.getElementById("valorinicial").value;
+    let valorinicial2 = parseInt(valorinicial);
+
+    ++contador;
+    ++i;
+
+
+    if (box1valor.trim() !== '') {
+      dados.push([box1valor, valorinicial2]); // Adiciona um novo item com 2 horas (ou o valor desejado)
+      desenharGrafico(); // Redesenha o gráfico com os dados atualizados
+      document.getElementById('box1').value = ''; // Limpa o campo de entrada
+      document.getElementById('box2').value = ''; // Limpa o campo de entrada
+      if (datavalor == "2") { datavalor = "1" };
+      data5.dataset.aberto = datavalor;
+      let novaatividade = `<div id="${contador}" class="tarefas">
           <button class="marcar"><box-icon name='radio-circle-marked'></box-icon></button>
           <tex-box class="texto">${input}</tex-box>
-          <button class="deletar"><box-icon name='trash' color='#f9f9f9' ></box-icon></button>
+          <button id="${i}" onclick="deletar(${contador})" class="deletar"><box-icon name='trash' color='#f9f9f9' ></box-icon></button>
           </div>`;
 
-    main.innerHTML += novaatividade;
+      main.innerHTML += novaatividade;
+    }
   }
-}
 
 // Função para desenhar o gráfico
 function desenharGrafico() {
