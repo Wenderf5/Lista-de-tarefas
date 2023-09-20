679564f6c0e54f8bca100ad18903bb90c9c0f23b
wenderf5 <wenderfabianoalves@gmail.com>
2023-09-20 17:25:49 -0300

    1.2

--- Script/script.js
+++ Script/script.js
@@ -7,10 +7,13 @@ var dados = [
   ['Tarefa', 'Horas por Dia'],
 ];
 
+let contador2 = 1;
+
 function deletar(id) {
   var tarefa = document.getElementById(id);
   tarefa.remove();
-
+  --contador2;
+  ax5();
   deletararray(id);
 }
 function deletararray(id) {
@@ -20,6 +23,8 @@ function deletararray(id) {
 
 let contador = 0;
 let i = 0;
+let ValorMaxRanger = document.getElementById("valorinicial");
+let ValorMaxRangerReal = ValorMaxRanger.max;
 
 
   // Função para adicionar um novo item ao array de dados
@@ -29,25 +34,46 @@ let i = 0;
     let main = document.getElementById("arealista");
     let valorinicial = document.getElementById("valorinicial").value;
     let valorinicial2 = parseInt(valorinicial);
+    let ValorMaxRanger = document.getElementById("valorinicial");
+    let ValorMaxRangerReal = ValorMaxRanger.max;
+    let resultado = ValorMaxRangerReal - valorinicial2;
+    ValorMaxRanger.max = resultado;
 
+    ++contador2;
     ++contador;
     ++i;
 
-
-    if (box1valor.trim() !== '') {
+    if (box1valor.trim() !== '' && input !== '' && valorinicial2 !== 0) {
       dados.push([box1valor, valorinicial2]); // Adiciona um novo item com 2 horas (ou o valor desejado)
       desenharGrafico(); // Redesenha o gráfico com os dados atualizados
       document.getElementById('box1').value = ''; // Limpa o campo de entrada
       document.getElementById('box2').value = ''; // Limpa o campo de entrada
+      document.getElementById('valorinicial').value = 0;
+      document.getElementById('valor').textContent = '0';
+
+
       if (datavalor == "2") { datavalor = "1" };
       data5.dataset.aberto = datavalor;
-      let novaatividade = `<div id="${contador}" class="tarefas">
-          <button class="marcar"><box-icon name='radio-circle-marked'></box-icon></button>
+      let novaatividade =`<div id="${contador}" class="tarefas">
+          <div class="marcar"><box-icon class="marcar" name='radio-circle-marked'></box-icon></div>
           <tex-box class="texto">${input}</tex-box>
-          <button id="${i}" onclick="deletar(${contador})" class="deletar"><box-icon name='trash' color='#f9f9f9' ></box-icon></button>
+          <button id="${i}" onclick="deletar(${contador})" id="delete" class="deletar"><box-icon name='trash' color='#f9f9f9' ></box-icon></button>
           </div>`;
 
       main.innerHTML += novaatividade;
+      ax5();
+    }
+  }
+
+  function ax5(){
+    if (contador2 >= 2 ){
+      imgadd.style.display = "none";
+      txt18.style.display = "none";
+      imggrafico.style.display = "none";
+    } else if (contador2 = 1){
+      imgadd.style.display = "block";
+      txt18.style.display = "block";
+      imggrafico.style.display = "block";
     }
   }
 
